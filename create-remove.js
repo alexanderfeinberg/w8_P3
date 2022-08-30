dcfb42849cc884d094556bb3b2f249e9b02eca66
alexander1fein <feinberg.alexander@gmail.com>
2022-08-30 14:02:05 -0400

    Done

--- create-remove.js
+++ create-remove.js
@@ -9,14 +9,26 @@ add.addEventListener("click", async () => {
 
         /*--------------- Get breed (Hint: Parse from URL) ---------------- */
         // Your code here
+        const breed = url.split('/')[4]
 
         /*------------ Create new dog card with the url above ------------- */
         /* (use the HTML structure for the current dog image in the index.html
             file to create a new image with the url) */
         // Your code here
+        const dogStruct = `
+                <figure>
+                    <img src="${url}" />
+                    <figcaption>${breed}</figcaption>
+                </figure>
+        `
 
         /* Add the new dog card as a child to the ul in the .gallery element */
         // Your code here
+        const uList = document.body.querySelector('section.gallery > ul')
+        const newElement = document.createElement('li')
+        newElement.innerHTML = dogStruct
+        uList.appendChild(newElement)
+
 
     } catch (e) {
         console.log("Couldn't fetch dog :(")
@@ -28,9 +40,11 @@ const removeFirst = document.getElementById("remove-first");
 removeFirst.addEventListener("click", () => {
     /*-------------------- Select the first dog card --------------------- */
     // Your code here
+    const first = document.querySelectorAll("ul li")[0]
 
     /*-------------------- Remove the first dog card --------------------- */
     // Your code here
+    first.remove()
 });
 
 /************************** REMOVE LAST DOG BUTTON ***************************/
@@ -38,7 +52,12 @@ const removeLast = document.getElementById("remove-last");
 removeLast.addEventListener("click", () => {
     /*-------------------- Select the last dog card ----------------------- */
     // Your code here
+    let last = document.querySelectorAll("ul li")
+    last = last[last.length-1]
+
+
 
     /*-------------------- Remove the last dog card ----------------------- */
     // Your code here
+    last.delete()
 });
